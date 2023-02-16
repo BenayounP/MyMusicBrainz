0cb313ed27e1e352e303e42bcb4386ab97d06915
Piotr2293 <pierre@cabnum.fr>
2023-02-16 17:55:49 +0100

    little search ui improvement

--- app/src/main/java/eu/benayoun/mymusicbrainz/ui/compose/screens/home/composables/SearchArtistTextField.kt
+++ app/src/main/java/eu/benayoun/mymusicbrainz/ui/compose/screens/home/composables/SearchArtistTextField.kt
@@ -2,18 +2,23 @@ package eu.benayoun.mymusicbrainz.ui.compose.screens.home.composables
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
 import eu.benayoun.mymusicbrainz.core.designsystem.theme.ComposeDimensions.padding2
 import eu.benayoun.mymusicbrainz.core.designsystem.theme.ComposeDimensions.padding4
 import eu.benayoun.mymusicbrainz.core.designsystem.theme.ComposeDimensions.padding8
 
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 fun SearchArtistTextField(modifier: Modifier = Modifier, onButtonClick: (query: String) -> Unit) {
     val textState = remember { mutableStateOf(TextFieldValue()) }
@@ -25,6 +30,13 @@ fun SearchArtistTextField(modifier: Modifier = Modifier, onButtonClick: (query: 
     ) {
 
         val maxCharInTextField = 30
+        val keyboardController = LocalSoftwareKeyboardController.current
+
+        fun search() {
+            keyboardController?.hide()
+            onButtonClick(textState.value.text)
+        }
+
         OutlinedTextField(
             modifier = Modifier
                 .weight(1f)
@@ -35,14 +47,19 @@ fun SearchArtistTextField(modifier: Modifier = Modifier, onButtonClick: (query: 
             label = { Text("Search artist") }, // todo replace with a string in xml
             placeholder = { Text("Artist name") }, // todo replace with a string in xml
             value = textState.value,
-            onValueChange = { if (it.text.length <= maxCharInTextField) textState.value = it }
+            onValueChange = { if (it.text.length <= maxCharInTextField) textState.value = it },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(
+                onDone = { search() })
         )
         Button(modifier = Modifier
             .wrapContentSize()
             .padding(start = padding4, top = padding8)
             .fillMaxHeight(),
             shape = RoundedCornerShape(padding2),
-            onClick = { onButtonClick(textState.value.text) }) {
+            onClick = {
+                search()
+            }) {
             Text("SEARCH") // todo replace with a string in xml
         }
     }
